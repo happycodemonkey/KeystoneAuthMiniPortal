1e1875cb2dfdfb3e532e9662cc2cef9e6196e718
Carrie Arnold <cmarnold@tacc.utexas.edu>
2017-04-20 16:41:48 -0500

    First commit

---
+++ keystoneauth/urls.py
@@ -0,0 +1,27 @@
+"""keystoneauth URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from openstack_auth import utils
+from openstack_auth import views
+
+utils.patch_middleware_get_user()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/', include('userProfile.urls')),
+    url(r'', include('openstack_auth.urls')),
+]
